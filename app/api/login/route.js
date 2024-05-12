c9b51e5c99179c4ce180d5d818ba020b996721ef
at0mic-l1mbo <augustolazer@gmail.com>
2024-05-12 20:15:20 -0300

    full back-end implementation

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -1,13 +1,33 @@
 import { PrismaClient } from '@prisma/client'
 import { NextResponse } from 'next/server'
+const bcrypt = require('bcrypt');
 
-export async function GET(req){
+export async function GET(req) {
     const prisma = new PrismaClient()
-    try{
+    try {
         const users = await prisma.user.findMany();
-        return NextResponse.json({message: 'OK', users})
-    }catch(err){
-        return NextResponse.json({message: "Error", err}, {status: 400})
+        return NextResponse.json({ message: 'OK', users })
+    } catch (err) {
+        return NextResponse.json({ message: "Error", err }, { status: 400 })
     }
-   
+}
+
+export async function POST(req) {
+    const prisma = new PrismaClient();
+    try {
+        const { email, password } = await req.json();
+        const user = await prisma.user.findUnique({
+            where: { email: email }
+        });
+        if (!user) {
+            return NextResponse.json({ message: "Usuário não encontrado" }, { status: 404 });
+        }
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (!passwordMatch) {
+            return NextResponse.json({ message: "Credenciais inválidas" }, { status: 401 });
+        }
+        return NextResponse.json({ message: 'Usuário autenticado com sucesso', user });
+    } catch (error) {
+        return NextResponse.json({ message: "Erro interno do servidor" }, { status: 500 });
+    }
 }
